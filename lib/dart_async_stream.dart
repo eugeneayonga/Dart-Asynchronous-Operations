5d899cbf35efb8f254aefe0e1a03e614a74784e6
Eugene <eugeneayonga@gmail.com>
2023-05-14 20:16:28 +0300

    Dart streams and Futures

--- lib/dart_async_stream.dart
+++ lib/dart_async_stream.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 void main(List<String> arguments) {
-  Stream.periodic(const Duration(seconds: 5), (x) => x).listen((print));
-  Stream.periodic(const Duration(seconds: 10), (x) => -x).listen((print));
+  // Stream.periodic(const Duration(seconds: 1), (x) => x).listen((print));
+  // Stream.periodic(const Duration(seconds: 2), (x) => -x).listen((print));
+
+  Stream.fromFutures([Future(() => 3), Future.value(2)]).listen((print));
 }
